d61842b905e77acd1a75a1d5df38370ac547c50a
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2025-02-27 11:09:48 -0300

    a

--- api.py
+++ api.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 import psycopg2
 import os
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite requisições de qualquer origem (mude se precisar)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
+
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://vagas_1ukx_user:gyKqtHfa24cGoojv7SYvVrfbUciqxZtq@dpg-cv06f13tq21c73920oc0-a.oregon-postgres.render.com/vagas_1ukx")
 
